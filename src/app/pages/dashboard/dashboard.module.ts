3dfe5ad5243957023fdcee9e588112ec8981963b
Ramiandrisoa1 <ramiandrisoamicka@gmail.com>
2022-03-26 17:57:04 +0300

    fix add product page

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -1,58 +1,30 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-// import { AuthGuard } from 'src/app/services/auth.guard.service';
 import { DahsboardPageComponent } from './dashboard.page';
+import {
+  FormGroupDirective,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 const routes: Routes = [
   {
     path: '',
     component: DahsboardPageComponent,
-    // canActivate: [AuthGuard],
-    // children: [
-    //   {
-    //     path: 'page1',
-    //     loadChildren: () =>
-    //       import('src/app/pages/page1/page1.module').then(
-    //         (m) => m.Page1PagesModule
-    //       ),
-    //   },
-    // {
-    //   path: 'commandes',
-    //   loadChildren: () =>
-    //     import(
-    //       'src/app/pages/users-conneted/user-commandes/user-commandes.module'
-    //     ).then((m) => m.UserCommandePagesModule),
-    // },
-    // {
-    //   path: 'evenements',
-    //   loadChildren: () =>
-    //     import(
-    //       'src/app/pages/users-conneted/user-events/user-events.module'
-    //     ).then((m) => m.UserEventsPagesModule),
-    // },
-    // {
-    //   path: 'profils',
-    //   loadChildren: () =>
-    //     import(
-    //       'src/app/pages/users-conneted/user-profils/user-profils.module'
-    //     ).then((m) => m.UserProfilsPagesModule),
-    // },
-    // ],
   },
 ];
 
 @NgModule({
   imports: [
-    CommonModule, // ngrx related imports
-
-    // SidebarModule,
-
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     RouterModule.forChild(routes),
     // AppProductComponentModule,
   ],
 
   declarations: [DahsboardPageComponent],
-  providers: [],
+  providers: [FormGroupDirective],
 })
 export class DahsboardPagesModule {}
--- src/app/pages/dashboard/dashboard.page.ts
+++ src/app/pages/dashboard/dashboard.page.ts
@@ -1,53 +1,47 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { Subject } from 'rxjs';
-import { tap } from 'rxjs/operators';
-// import { EventService } from 'src/app/services/event.service';
-// import { LocalstorageService } from 'src/app/services/localstorage-service';
+import { AngularFirestore } from '@angular/fire/firestore';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.page.html',
   styleUrls: ['./dashboard.page.scss'],
 })
 export class DahsboardPageComponent implements OnInit {
-  public name = '';
-  public start = 0;
-  public limit = 10;
-  public user_Type!: number;
-  destroy$: Subject<boolean> = new Subject<boolean>();
-
   constructor(
-    // public storage: LocalstorageService,
-    // public eventService: EventService,
-    private router: Router
-  ) {
-    // this.eventService.checkUserLogged$
-    //   .pipe(
-    //     tap(
-    //       (response: any) => {
-    //         if (response.current_user && response.current_user.response) {
-    //           this.name = response.current_user.response.user.name;
-    //           this.user_Type =
-    //             response.current_user.response.user.users_type.id;
-    //         }
-    //         if (response.current_user && response.current_user.user) {
-    //           this.name = response.current_user.user.name;
-    //           this.user_Type = response.current_user.user.users_type.id;
-    //         }
-    //         if (response && response.user) {
-    //           this.name = response.user.name;
-    //           this.user_Type = response.user.users_type.id;
-    //         }
-    //       },
-    //       (err) => {
-    //         console.log('err', err);
-    //       }
-    //     )
-    //   )
-    //   .subscribe();
-  }
+    private db: AngularFirestore,
+    public form: FormGroup,
+    private formBuilder: FormBuilder
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      price: ['', [Validators.required]],
+    });
+  }
+  get f(): { [key: string]: AbstractControl } {
+    return this.form.controls;
+  }
+  public addProduct() {
+    let body = {
+      nomProduit: this.form.value.name,
+      prixProduit: this.form.value.price,
+    };
+    this.db
+      .collection('product')
+      .add(body)
+      .then((docRef) => {
+        console.log('Document written with ID: ', docRef.id);
+      })
+      .catch((error) => {
+        console.error('Error adding document: ', error);
+      });
+  }
 
   ngOnDestroy() {}
 }
